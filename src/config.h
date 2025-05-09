18e9a03026ec40d9bacf7d815526b43bb24311e0
azatal <lukasz.stodolka100@gmail.com>
2025-05-10 00:23:23 +0200

    generating move added

--- src/config.h
+++ src/config.h
@@ -1,8 +1,20 @@
+// cosnt 
+constexpr i32 INF = 1e9 + 7;
+
+// data types
 typedef unsigned long long u64;
-typedef short i8;
 typedef unsigned int u32;
+typedef int i32;
 typedef unsigned short u8;
-#define U64(x) static_cast<u64>(x)
-#define U8(x) static_cast<u8>(x)
+
+// enums
 enum class Piece { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
 enum class Color { WHITE, BLACK };
+
+// methods for enums
+inline Color opposite(Color c){
+    return c == Color::WHITE ? Color::BLACK : Color::WHITE;
+}
+// makro
+#define U64(x) static_cast<u64>(x)
+#define U8(x) static_cast<u8>(x)
